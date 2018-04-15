36cb84f45090a69d566f8cf2fe0a20b3cfc5db23
Hemanth Devarapalli <itsmehemant123@gmail.com>
2018-04-15 15:58:36 -0400

    Logging more info;

--- parse.py
+++ parse.py
@@ -9,11 +9,17 @@ logging.basicConfig(level=logging.INFO)
 with open('./config/config.json') as data_file:
     config = json.load(data_file)
 
+logging.info('Finished parsing config.')
+
 handle = MongoHandle(config)
+logging.info('Initialized the Mongo connection.')
+
 t = Twarc(config['twitter']['consumer_key'], config['twitter']['consumer_secret'],
           config['twitter']['access_token'], config['twitter']['access_token_secret'])
+logging.info('Initialized Twitter connection.')
 
 for source_file in os.listdir('./' + config['source_folder']):
+    logging.info('Preparing to hydrate: ' + source_file)
     tweet_ids = open('./' + config['source_folder'] + '/' + source_file)
     new_tweet_ids = []
     for line in tweet_ids:
@@ -24,4 +30,5 @@ for source_file in os.listdir('./' + config['source_folder']):
     tweet_ids.close()
     logging.info('Finished hydrating: ' + source_file)
 
+logging.info('Finished hydration task.')
 handle.clean()
